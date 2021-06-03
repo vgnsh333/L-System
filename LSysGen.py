a21fd1c8873498fbb914ecacd8f2e1b26eee328a
Vignesh <43577358+vgnsh333@users.noreply.github.com>
2021-06-03 20:10:31 +0530

    updates in lsysgen

--- LSysGen.py
+++ LSysGen.py
@@ -1,23 +1,23 @@
-axiom = "F+F+F+F"
-rule = 'F+F-F-FF+F+F-F'
-iterations=2
-next_gen = ""
-
-#print (next_gen)
-
-def lsysgen(axiom,rule,iterations):
+axiom = "FX"
+iterations=5
+rules = {
+    "X":">[-FX]+FX"
+}
+def lsysgen(axiom,rules,iterations):
     next_gen = ''
     axiom=axiom
-    rule=rule
     for i in range(iterations):
         next_gen=''
         for x in axiom:
-            if x == "F":
-                next_gen+=rule
+            if x in rules:
+                print(x,rules[x])
+                next_gen+=rules[x]
             else:
+                #print(x)
                 next_gen+=x
             axiom = next_gen
-    if iterations ==0:
+    if iterations==0:
         next_gen=axiom 
     print(next_gen)
     return next_gen
+lsysgen(axiom,rules,iterations)
