66d9e10b11e799cb3305acb307a4290ebea4a05d
Vignesh <43577358+vgnsh333@users.noreply.github.com>
2021-06-03 21:20:50 +0530

    code refactor

--- LSysGen.py
+++ LSysGen.py
@@ -1,16 +1,19 @@
-axiom = "FX"
-iterations=5
-rules = {
-    "X":">[-FX]+FX"
-}
-def lsysgen(axiom,rules,iterations):
+
+def rules_formatter(rules_unformatted):
+    rules = {}
+    for i in rules_unformatted:
+        x = i.replace(' ','').split('->')
+        rules[x[0]]=x[1]
+    return rules
+def lsysgen(axiom,rules_unformatted,iterations):
     next_gen = ''
+    rules=rules_formatter(rules_unformatted)
     axiom=axiom
     for i in range(iterations):
         next_gen=''
         for x in axiom:
             if x in rules:
-                print(x,rules[x])
+                #print(x,rules[x])
                 next_gen+=rules[x]
             else:
                 #print(x)
@@ -18,6 +21,7 @@ def lsysgen(axiom,rules,iterations):
             axiom = next_gen
     if iterations==0:
         next_gen=axiom 
-    print(next_gen)
+    #print(next_gen)
     return next_gen
-lsysgen(axiom,rules,iterations)
+
+
---
+++ draw.py
@@ -0,0 +1,67 @@
+import turtle
+from LSysGen import lsysgen
+def create_turtle(screen_height,screen_width,speed,heading,x,y,size):
+    turtle.screensize(screen_width,screen_height)
+    oogway = turtle.Turtle()
+    oogway.speed(speed)
+    oogway.penup()
+    oogway.setheading(heading)
+    oogway.setx(x)
+    oogway.sety(y)
+    oogway.pendown()
+    oogway.resizemode('auto')
+    oogway.turtlesize(size)
+    return oogway
+angle = 60
+axiom = "YF"
+rules = {
+'X -> YF+XF+Y',
+'Y -> XF-YF-X'
+}
+def draw(oogway,iterations,line_length,angle,axiom,rules):
+    iterations = 5
+    line_length = 15
+    oogway.fillcolor('blue')
+    drawStr = lsysgen(axiom,rules,iterations)
+    stack = []
+    for i in drawStr:
+        if i == "F":
+            oogway.pendown()
+            oogway.forward(line_length)
+        elif i == "f":
+            oogway.penup()
+            oogway.forward(line_length)
+        elif i == "|":
+            oogway.penup()
+            oogway.left(180)
+        elif i == "[":
+            stack.append((oogway.heading(),oogway.position()))
+        elif i == "]":
+            oogway.penup()
+            heading,position = stack.pop()
+            oogway.goto(position)
+            oogway.setheading(heading)
+            oogway.pendown()
+        elif i == "#":
+            pass
+        elif i == "{":
+            oogway.begin_fill()
+        elif i == "}":
+            oogway.end_fill()
+        elif i == "<":
+            pass
+        elif i == ">":
+            line_length *= 1
+        elif i == "&":
+            pass
+        elif i == "(":
+            pass
+        elif i == ")":
+            pass
+        elif i == "+":
+            oogway.left(angle)
+        elif i == "-":
+            oogway.right(angle)
+        else:
+            pass
+    turtle.done()
