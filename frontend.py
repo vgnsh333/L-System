5f6bdffc43e37b51a547ec9309b0fafc3528776e
Vignesh <43577358+vgnsh333@users.noreply.github.com>
2021-06-03 23:55:56 +0530

    dq

---
+++ frontend.py
@@ -0,0 +1,15 @@
+# frontend.py
+
+import streamlit as st
+from backend import *
+
+st.title("Turtle App")
+title = st.text_input("Canvas Title", value="My Canvas")
+width = st.number_input("Canvas Width", value=500)
+height = st.number_input("Canvas Height", value=500)
+length = st.number_input("Square Length", value=200)
+
+clicked = st.button("Paint")
+
+if clicked:
+    canvas_builder(title, width, height, length)
